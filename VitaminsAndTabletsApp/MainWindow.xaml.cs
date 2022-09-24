8ab78abbfaa382bdecfaa95150547af4eeb8b6d7
Andrew285 <andriyk116@gmail.com>
2022-09-24 19:57:03 +0300

    create basic design on auth page

--- VitaminsAndTabletsApp/MainWindow.xaml.cs
+++ VitaminsAndTabletsApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace VitaminsAndTabletsApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
